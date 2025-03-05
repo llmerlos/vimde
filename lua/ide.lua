f749de9b8a211e8fc25a11bfec95d35c4060e469
Lluis Merlos Pieri <llmerlos@outlook.com>
2025-03-05 14:26:18 +0100

    cleanup

--- lua/ide.lua
+++ lua/ide.lua
@@ -40,17 +40,6 @@ require("lazy").setup({
 		},
 	},
 	{
-		"folke/zen-mode.nvim",
-		opts = {
-			plugins = {
-				tmux = { enabled = true },
-			},
-		},
-		keys = {
-			{ "<leader>qz", "<CMD>ZenMode<CR>", desc = "Toggle [z]en mode" },
-		},
-	},
-	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
 		opts = {
@@ -62,6 +51,7 @@ require("lazy").setup({
 				{ "<leader>f", group = "[f]ind" },
 				{ "<leader>g", group = "[g]it", mode = { "n", "v" } },
 				{ "<leader>q", group = "[q]ustom" },
+				{ "<leader>t", group = "[t]asks" },
 				{ "<leader>r", group = "[r]un" },
 				{ "<leader>v", group = "[v]imrc" },
 				{ "<leader>w", group = "[w]orkspace" },
@@ -135,15 +125,6 @@ require("lazy").setup({
 		},
 	},
 	{
-		"akinsho/toggleterm.nvim",
-		keys = {
-			{ "<leader>`", ":ToggleTerm direction=float<CR>", desc = "ToggleTerm" },
-		},
-		config = function()
-			require("toggleterm").setup()
-		end,
-	},
-	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
 		dependencies = {
@@ -163,7 +144,6 @@ require("lazy").setup({
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {
-			-- extensions = {'nvim-dap-ui'},
 			sections = {
 				lualine_c = {
 					{ "filename", path = 1 },
@@ -389,7 +369,6 @@ require("lazy").setup({
 		dependencies = {
 			-- Automatically install LSPs and related tools to stdpath for Neovim
 			-- Mason must be loaded before its dependents so we need to set it up here.
-			-- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
 			{ "williamboman/mason.nvim", opts = {} },
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
@@ -408,11 +387,6 @@ require("lazy").setup({
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 				callback = function(event)
-					-- NOTE: Remember that Lua is a real programming language, and as such it is possible
-					-- to define small helper and utility functions so you don't have to repeat yourself.
-					--
-					-- In this case, we create a function that lets us more easily define mappings specific
-					-- for LSP related items. It sets the mode, buffer and description for us each time.
 					local map = function(keys, func, desc, mode)
 						mode = mode or "n"
 						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
@@ -463,10 +437,6 @@ require("lazy").setup({
 						})
 					end
 
-					-- The following code creates a keymap to toggle inlay hints in your
-					-- code, if the language server you are using supports them
-					--
-					-- This may be unwanted, since they displace some of your code
 					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
 						map("<leader>cih", function()
 							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
@@ -482,15 +452,6 @@ require("lazy").setup({
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-			-- Enable the following language servers
-			--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-			--
-			--  Add any additional override configuration in the following tables. Available keys are:
-			--  - cmd (table): Override the default command used to start the server
-			--  - filetypes (table): Override the default list of associated filetypes for the server
-			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-			--  - settings (table): Override the default settings passed when initializing the server.
-			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				-- clangd = {},
 				lua_ls = {
@@ -505,19 +466,6 @@ require("lazy").setup({
 				},
 			}
 
-			-- Ensure the servers and tools above are installed
-			--
-			-- To check the current status of installed tools and/or manually install
-			-- other tools, you can run
-			--    :Mason
-			--
-			-- You can press `g?` for help in this menu.
-			--
-			-- `mason` had to be setup earlier: to configure its options see the
-			-- `dependencies` table for `nvim-lspconfig` above.
-			--
-			-- You can add other tools here that you want Mason to install
-			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				-- 'stylua', -- Used to format Lua code
@@ -601,25 +549,17 @@ require("lazy").setup({
 				--
 				-- No, but seriously. Please read `:help ins-completion`, it is really good!
 				mapping = cmp.mapping.preset.insert({
-					-- Select the [n]ext item
+					-- Select the [n]ext / [p]revious item
 					["<C-n>"] = cmp.mapping.select_next_item(),
-					-- Select the [p]revious item
 					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
 
 					-- Scroll the documentation window [b]ack / [f]orward
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 
-					-- Accept ([y]es) the completion.
-					--  This will auto-import if your LSP supports it.
-					--  This will expand snippets if the LSP sent a snippet.
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-
-					-- If you prefer more traditional completion keymaps,
-					-- you can uncomment the following lines
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<Tab>"] = cmp.mapping.select_next_item(),
-					["<S-Tab>"] = cmp.mapping.select_prev_item(),
 
 					-- Manually trigger a completion from nvim-cmp.
 					--  Generally you don't need this, because nvim-cmp will display
@@ -658,16 +598,6 @@ require("lazy").setup({
       { "<leader>dk", function() require("dap").step_out() end, desc = "[d]ebug step out [k]" },
       { "<leader>dl", function() require("dap").step_over() end, desc = "[d]ebug step over [l]" },
       { "<leader>dh", function() require("dap").pause() end, desc = "[d]ebug pause [h]" },
-
-      -- { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
-      -- { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
-      -- { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
-      -- { "<leader>dj", function() require("dap").down() end, desc = "Down" },
-      -- { "<leader>dk", function() require("dap").up() end, desc = "Up" },
-      -- { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
-      -- { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
-      -- { "<leader>dS", function() require("dap").session() end, desc = "Session" },
-      -- { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
     },
 
 		config = function()
@@ -740,8 +670,8 @@ require("lazy").setup({
 		opts = {},
 		keys = {
 			{ "<leader>b", "<CMD>OverseerRun Build<CR>", desc = "[b]uild" },
-			{ "<leader>qtl", "<CMD>OverseerOpen<CR><CMD>OverseerRun<CR>", desc = "[q]ustom [t]ask [l] launch" },
-			{ "<leader>qtu", "<CMD>OverseerToggle<CR>", desc = "[q]ustom [t]ask [u]i" },
+			{ "<leader>tl", "<CMD>OverseerOpen<CR><CMD>OverseerRun<CR>", desc = "[t]ask [l]aunch" },
+			{ "<leader>tu", "<CMD>OverseerToggle<CR>", desc = "[t]ask [u]i" },
 		},
 	},
 })
